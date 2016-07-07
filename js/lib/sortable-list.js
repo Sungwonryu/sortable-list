1f6b2f72f38a56a9b4ea10a87efe707e3c390d2f
Sungwon Ryu <sungwonryu@Syllabuss-MacBook-Pro.local>
2016-07-07 14:18:50 -0400

    Resovle the issue of save the changes in list
    Add this.tempItems, _updateItems(), _updateTempItems()
    . this.tempItems is a temporary storage for this.items
    . _updateItems() updates this.items same as this.tempItems
    . _updateTempItems() updates this.tempItems according to the changes on this.$listUl
    
    Add self._updateTempItems() as update: option of sortable() in _enableListSortable()
    
    Move jQuery show() calls to showList(), showDetail(), showDetailEdit()

--- js/lib/sortable-list.js
+++ js/lib/sortable-list.js
@@ -1,5 +1,5 @@
 /**
- *	sortable-list.js 
+ *	sortable-list.js
  *
  *  sortable-list.js depends on jquery and jquery.ui
  **/
@@ -10,6 +10,8 @@
 
 			// this.items stores the reference of array of objects such as favorites
 			this.items = options.items || [];
+			// this.tempItems is a temporary storage for this.items
+			this.tempItems = [];
 
 			// list section
 			this.listSectionId = options.listSectionId;
@@ -26,30 +28,31 @@
 			this.detailArticleId = options.detailArticleId;
 			this.detailEditArticleId = options.detailEditArticleId;
 
-			this.getDetailArticleHtml = options.getDetailArticleHtml;			
+			this.getDetailArticleHtml = options.getDetailArticleHtml;
 			this.getDetailEditArticleHtml = options.getDetailEditArticleHtml;
 
 			// data attribute of each li
 			this.listDataId = options.listDataId;
 
-			// default transition speed
-			this.defaultSpeed = options.defaultSpeed;
+			// default animation duration determining how long the animation will return
+			// for show, hide, fadeIn, fadeOut, etc
+			this.defaultDuration = options.defaultDuration;
 
 			// event listeners
 			this.events = options.events;
 
 			// customized methods
-			this.methods = options.methods;		
+			this.methods = options.methods;
 
 		}
 
 		SortableList.prototype = {
 
-			// After document is ready, init() is invoked 
+			// After document is ready, init() is invoked
 			// to create jQuery objects and add event listeners
 			init: function() {
-				
-				// Create jQuery objects by using HTML ids in options
+
+				// Create jQuery objects by using HTML ids and classes in options
 				this.$listSection = $('#' + this.listSectionId);
 				this.$listHeader = $('#' + this.listHeaderId);
 				this.$listUl = $('#' + this.listUlId);
@@ -59,31 +62,42 @@
 				this.$detailArticle = $('#' + this.detailArticleId);
 				this.$detailEditArticle = $('#' + this.detailEditArticleId);
 
-				// this.resetListSection() generates $lis by using the given options.items
-				this.resetListSection();
-				this._addEventListeners();	
+				this.showList();
+				// _updateTempItems makes this.tempItems copies reference elements in this.items
+				this._updateTempItems();
+				this._addEventListeners();
+
+			},
+
+			// Add eventListeners
+			_addEventListeners: function() {
+
+				var events = this.events;
+				for (var eventName in events) {
+					console.log(eventName + 'is added');
+					this.events[eventName].call(this);
+				}
 
 			},
 
 			// Create HTML elements and append them to this.$listHeader and this.$listUl
 			resetListSection: function() {
-				
+
 				var self = this;
-				
+
 				// Remove all existing child nodes of this.$listHeader
 				this.$listHeader.empty();
 				console.log('empty this.$listHeader with id="' + this.listHeaderId + '"');
-				
+
 				// Create a header and append it to this.$listHeader
 				this.$listHeader.append($(this.getListHeaderHtml()));
 
 				// Remove all existing child nodes of $ul
 				this.$listUl.empty();
-				console.log('empty this.$listUl with id="' + this.ulId + '"');
+				console.log('empty this.$listUl with id="' + this.listUlId + '"');
 
-				// Create an array of HTML li strings iterating through this.items
-				var arrayHtmlLis = this.items.map(function(item, index, items) {
-	
+				// Create an array of li HTML strings iterating through this.items
+				var arrayLiHtmls = this.items.map(function(item, index, items) {
 					var opts = {
 						item: item,
 						itemIndex: index,
@@ -93,209 +107,238 @@
 				});
 
 				// Create HTML lis and append them into this.$listUl
-				this.$listUl.append(arrayHtmlLis.join(''));
+				this.$listUl.append(arrayLiHtmls.join(''));
 
 			},
 
-			// Create HTML elements and append them to this.$detailArticle 
-			resetDetailArticle: function(itemIndex, speed) {
-				
+			// Create HTML elements and append them to this.$detailArticle
+			resetDetailArticle: function(itemIndex) {
+
 				var self = this;
-				speed = speed || this.defaultSpeed;
 
 				// Remove all existing child nodes of this.$detailArticle
 				this.$detailArticle.empty();
 
+				// Create detail article's HTML content
+				// and append it into this.$detailArticle
 				var opts = {
 					item: this.items[itemIndex],
 					itemIndex: itemIndex
 				};
-
 				var strDetailHtml = this.getDetailArticleHtml(opts);
-
 				this.$detailArticle.append($(strDetailHtml));
-				
-				// Show this.$detailSection and this.$detailArticle
-				this.$detailSection.show(speed);
-				this.$detailArticle.show(speed);
 
 			},
 
 			// Recreate HTML elements for this.$detailEditArticle
-			resetDetailEditArticle: function(itemIndex, speed) {
+			resetDetailEditArticle: function(itemIndex) {
 
 				var self = this;
-				speed = speed || this.defaultSpeed;
 
 				// Remove all existing child nodes of this.$detailEditArticle
 				this.$detailEditArticle.empty();
 
+				// Create detail edit article's HTML content
+				// and append it into this.$detailEditArticle
 				var opts = {
 					item: this.items[itemIndex],
 					itemIndex: itemIndex
 				};
-
 				var strDetailEditHtml = this.getDetailEditArticleHtml(opts);
-
 				this.$detailEditArticle.append($(strDetailEditHtml));
-
-				// Show this.$detailSection and this.$detailEditArticle
-				this.$detailSection.show(speed);
-				this.$detailEditArticle.show(speed);
 
 			},
 
-			// Find arrayMatchedLis which returns true from callback and then
-			// invoke action with the passing parameter arrayMatchedLis[i]
-			findLis: function(callback, action) {
+			// // Find arrayMatchedLis which return true from callback and then
+			// // invoke action with the passing parameter arrayMatchedLis[i]
+			// findLis: function(callback, action) {
+			//
+			// 	var self = this;
+			//
+			// 	// Turn an array-like jQuery object into an array
+			// 	var arrayLis = $.makeArray(this.$listUl.find('.' + this.listLiClass));
+			// 	var lisLength = arrayLis.length;
+			// 	var arrayMatchedLis = [];
+			// 	var isMatched;
+			//
+			// 	for (var i = 0; i < lisLength; i++) {
+			// 		isMatched = callback(arrayLis[i], i, arrayLis);
+			// 		if (isMatched) {
+			// 			// When callback return true, push arrayLis[i] to arrayMatchedLis
+			// 			arrayMatchedLis.push(arrayLis[i]);
+			// 		}
+			// 	}
+			//
+			// 	// When action function is given
+			// 	if (action) {
+			// 		var matchedLisLength = arrayMatchedLis.length;
+			// 		for (var i = 0; i < matchedLisLength; i++) {
+			// 			action.call(self, arrayMatchedLis[i], i, arrayMatchedLis);
+			// 		}
+			// 	}
+			//
+			// },
+			//
+			// // Find matchedLis which returns true from callback and then
+			// // remove the li and the corresponding item from this.items
+			// removeLis: function(callback) {
+			//
+			// 	var self = this;
+			//
+			// 	this.findLis(callback, function(matchedLi) {
+			// 		var $matchedLi = $(matchedLi);
+			// 		// Get itemId from the data-id attribute of li
+			// 		var itemId = $matchedLi.data(self.listDataId);
+			//
+			// 		// Remove the corresponding item from the this.items
+			// 		self.items.splice(itemId, 1);
+			// 		// Remove the matchedLi
+			// 		$matchedLi.remove();
+			//
+			// 	});
+			//
+			// },
+			//
+			//
+			// findLiById: function(itemIndex, action) {
+			//
+			// 	var self = this;
+			//
+			// 	this.findLis(function(li) {
+			// 		// typeof id is number
+			// 		var id = $(li).data(self.listDataId);
+			// 		return id === itemIndex;
+			// 	}, function(matchedLi) {
+			// 		action.call(self, matchedLi);
+			// 	});
+			//
+			// },
+			//
+			// removeLiById: function(itemIndex) {
+			// 	var self = this;
+			//
+			// 	this.findLiById(itemIndex, function(matchedLi) {
+			// 		// Remove the selected li
+			// 		$(matchedLi).remove();
+			// 		// Remove the corresponding item from this.items
+			// 		self.items.splice(itemIndex, 1);
+			// 	});
+			// },
 
-				var self = this;
+			// Update this.items same as this.tempItems
+			_updateItems: function(callback) {
 
-				// Turn an array-like jQuery object into an array
-				var arrayLis = $.makeArray(this.$listUl.find('.' + this.listLiClass));
+				var items = this.items;
+				var tempItems = this.tempItems;
+				var tempItemsLength = tempItems.length;
 
-				var length = arrayLis.length;
-				var arrayMatchedLis = [];
-				var isMatched;
-
-				for (var i = 0; i < length; i++) {
-					isMatched = callback(arrayLis[i], i, arrayLis);
-					if (isMatched) {
-						// When callback return true, push arrayLis[i] to arrayMatchedLis
-						arrayMatchedLis.push(arrayLis[i]);
-					}	
+				// Copy the array elements from tempItems to items
+				for (var i = 0; i < tempItemsLength; i++) {
+					items[i] = tempItems[i];
 				}
-				// When action function is given 
-				if (action) {
-					var length = arrayMatchedLis.length;
-					for (var i = 0; i < length; i++) {
-						action.call(self, arrayMatchedLis[i], i, arrayMatchedLis);
+				// Removed the elements of items which have been removed from tempItems
+				items.splice(tempItemsLength);
+
+				if (callback) {
+					var itemsLength = items.length;
+					for (var i = 0; i < itemsLength; i++) {
+						callback.call(this, items[i], i, items);
 					}
 				}
 
 			},
-			
-			// Find matchedLis which returns true from callback and then
-			// remove the li and the corresponding item from this.items 
-			removeLis: function(callback) {
-				
-				var self = this;
 
-				this.findLis(callback, function(matchedLi) {
-					var $matchedLi = $(matchedLi);
-					// Get itemId from the data-id attribute of li
-					var itemId = $matchedLi.data(self.listDataId);
-					// Remove the corresponding item from the this.items
-					self.items.splice(itemId, 1);
-					// Remove the matchedLi
-					$matchedLi.remove();
-				})
-			
-			},
-	
+			// Update this.tempItems according to the changes on this.$listUl
+			_updateTempItems: function(callback) {
 
-			findLiById: function(itemIndex, action) {
-			
-				var self = this;
-
-				this.findLis(function(li) {
-					// typeof id is number
-					var id = $(li).data(self.listDataId);
-					return id === itemIndex;
-				}, function(matchedLi) {
-					action.call(self, matchedLi);					
-				});
-			
-			},
-
-			removeLiById: function(itemIndex) {
-				var self = this;
-			
-				this.findLiById(itemIndex, function(matchedLi) {
-					// Remove the selected li 
-					$(matchedLi).remove();
-					// Remove the corresponding item from this.items
-					self.items.splice(itemIndex, 1);
-				});
-			},
-
-			// Iterate this.$listUl and update and save this.items
-			saveListEdit: function() {
-
-				var self = this;
-
-				// Turn an array-like jQuery object into an array
-				var lis = $.makeArray(this.$listUl.find('.' + this.listLiClass));
-				// newItems will store the references of the rearranged items
-				var newItems = [];
 				var items = this.items;
-				var length = lis.length;
+				var tempItems = this.tempItems;
+				var arrayLis = $.makeArray(this.$listUl.find('.' + this.listLiClass));
+				var lisLength = arrayLis.length;
+				var listDataId = this.listDataId;
 
-				for (var i = 0; i < length; i++) {
-					// itemIndex stores the this.listDataId data attribute of each rearranged li
-					var itemIndex = $(lis[i]).data(this.listDataId);
-					// Push the reference of each rearranged item
-					newItems.push(items[i]);
+				for (var i = 0; i < lisLength; i++) {
+					// itemIndex stores the listDataId data attribute of each li
+					var itemIndex = $(arrayLis[i]).data(listDataId);
+					// Copy the reference of each rearranged item
+					tempItems[i] = items[itemIndex];
 				}
-				// Make this.items same as newItems
-				for (var i = 0; i < length; i++) {
-					items[i] = newItems[i];
-				}
-				// Splice the removed items from this.items
-				items.splice(length);
+				// Removed the elements of items which have been removed from this.$listUl
+				tempItems.splice(lisLength);
 
-				// 
-				// TODO: Save the updated this.items to database
-				// 
+				if (callback) {
+					var tempItemsLength = tempItems.length;
+					for (var i = 0; i < tempItemsLength; i++) {
+						callback.call(this, tempItems[i], i, tempItems);
+					}
+				}
 
 			},
 
+			// Update this.items same as this.tempItems and save this.items into database
+			saveListEdit: function(callback) {
+
+				this._updateItems();
+
+				//
+				// TODO: Save the updated this.items to database
+				//
+
+			},
+
+			// Update one element of this.items accroding to the changes from the form inputs
+			// and save this.items into database
 			saveDetailEdit: function(itemIndex) {
 
 				var item = this.items[itemIndex];
 
 				// Turn an array-like jQuery object into an array
 				var arrayInputs = $.makeArray(this.$detailEditArticle.find('input'));
-				var length = arrayInputs.length;
+				var inputsLength = arrayInputs.length;
 
-				// Update the properties of the selected item 
+				// Update the properties of the selected item
 				// with input values of the form in #detailEditArticle
-				for (var i = 0; i < length; i++) {
+				for (var i = 0; i < inputsLength; i++) {
 					var $input = $(arrayInputs[i]);
 					var prop = $input.attr('name');
 					item[prop] = $input.val();
 				}
 
+				// Turn an array-like jQuery object into an array
 				var arrayTextareas = $.makeArray(this.$detailEditArticle.find('textarea'));
-				length = arrayTextareas.length;
+				textareasLength = arrayTextareas.length;
 
-				// Update the properties of the selected item 
+				// Update the properties of the selected item
 				// with textarea values of the form in #detailEditArticle
-				for (var i = 0; i < length; i++) {
+				for (var i = 0; i < textareasLength; i++) {
 					var $textarea = $(arrayTextareas[i]);
 					var prop = $textarea.attr('name');
 					item[prop] = $textarea.val();
 				}
 
-				// 
+				//
 				// TODO: Save the updated item to database
-				// 
+				//
 			},
 
 			// Make this.$ul sortable
-			// callback is invoked whenever there is any changes on the order of this.$ul 
+			// callback is invoked whenever there is any changes on the order of this.$ul
 			_enableListSortable: function(callback) {
 
 				var self = this;
-				
+
 				this.$listUl.sortable({
 					disabled: false,
-					// update: function(callback) {
-					// }
+					update: function() {
+						// Update this.tempItems according to the changes on this.$listUl
+						self._updateTempItems();
+						if (callback) {
+							callback.call(self)
+						}
+					}
 				});
 				// Disable selection of text content in this.$ul
 				this.$listUl.disableSelection();
-
 				console.log('SortableList is enabled');
 
 			},
@@ -309,86 +352,80 @@
 
 				// Enable selection of text content in this$.ul
 				this.$listUl.enableSelection();
-
 				console.log('SortableList is disabled');
 
 			},
 
-			// Add eventListeners
-			_addEventListeners: function() {
+			_hideList: function(duration) {
 
-				var events = this.events;
-				for (var eventName in events) {
-
-					console.log(eventName + 'is added');				
-					
-					this.events[eventName].call(this);
-				}
-
-				console.log('eventListeners are added');
-			
-			},
-
-			_hideList: function() {
-
-				this.$listSection.hide();
+				duration = duration || this.defaultDuration;
+				this.$listSection.hide(duration);
 				console.log('_hideList() is called');
-			
+
 			},
 
-			_hideDetail: function() {
-			
-				this.$detailArticle.hide();
+			_hideDetail: function(duration) {
+
+				duration = duration || this.defaultDuration;
+				this.$detailArticle.hide(duration);
 				console.log('_hideDetail() is called');
-			
-			},
-			
-			_hideDetailEdit: function() {
-			
-				this.$detailEditArticle.hide();
-				console.log('_hideDetailEdit() is called');
-			
+
 			},
 
-			showList: function() {
-			
+			_hideDetailEdit: function(duration) {
+
+				duration = duration || this.defaultDuration;
+				this.$detailEditArticle.hide(duration);
+				console.log('_hideDetailEdit() is called');
+
+			},
+
+			// Reset HTML list contents and display the list section
+			showList: function(opts) {
+
+				opts = opts || {};
+				var duration = opts.duration || this.defaultDuration;
 				this._hideDetail();
 				this._hideDetailEdit();
 				this.resetListSection();
-				this.$listSection.show();
-			
+				this.$listSection.show(duration);
+
 			},
 
-			// Display the detail of the selected item 
-			showDetail: function(itemIndex) {
+			// Reset HTML detail contents and display the detail article
+			showDetail: function(opts) {
+
+				var itemIndex = opts.itemIndex;
+				var duration = opts.duration || this.defaultDuration;
 
 				this._hideList();
 				this._hideDetailEdit();
-				// Set data(this.listDataId) on this.$detailSection
-				this.$detailSection.data(this.listDataId, itemIndex);
 
+				// Set data(this.listDataId) on this.$detailSection
+				this.$detailSection.data(this.listDataId, itemIndex)
 				console.log('this.$detailSection.data-' + this.listDataId + '= ' + this.$detailSection.data(this.listDataId));
-				
+
 				this.resetDetailArticle(itemIndex);
 
+				// Show this.$detailSection and this.$detailArticle
+				this.$detailSection.show(duration);
+				this.$detailArticle.show(duration);
 			},
 
 			// Display the edit form for the previously chosen favorite detail
-			showDetailEdit: function(itemIndex) {
+			showDetailEdit: function(opts) {
 
-				var self = this;
-				
+				var itemIndex = opts.itemIndex || this.$detailSection.data(this.listDataId);
+				var duration = opts.duration || this.defaultDuration;
+
 				this._hideList();
-				this._hideDetail();	
-				// this.$detailEditArticle.empty();
+				this._hideDetail();
 
-				// var opts = {
-				// 	item: this.items[itemIndex],
-				// 	itemIndex: itemIndex
-				// };
-				// this.$detailEditArticle.append($(this.getDetailEditArticleHtml(opts)));
 				this.resetDetailEditArticle(itemIndex);
-				this.$detailEditArticle.show();
+
+				// Show this.$detailSection and this.$detailEditArticle
+				this.$detailSection.show(duration);
+				this.$detailEditArticle.show(duration);
 			}
 
 		};
