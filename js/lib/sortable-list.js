a16d005c412290037303c8f71805d1cada6ebc22
Sungwon Ryu <sungwonryu@Syllabuss-MacBook-Pro.local>
2016-07-15 14:26:05 -0400

    Add uploadItems() and downloadItems()
    sortable-list.js
    . Change reset functions, setBothItems()
    . Change updateTempItems() saveListToTempItems()
    . Add saveDetailTempItems()
    
    index.html
    . Add uploadItems() and downloadItems()
    . Add saveListEdit() and saveDetailEdit() which utilize uploadItems() and downlodItems()

--- js/lib/sortable-list.js
+++ js/lib/sortable-list.js
@@ -9,9 +9,7 @@
 (function(window, $) {
 
 	function SortableList(options) {
-
 		return new SortableList.create(options)
-
 	}
 
 	/**
@@ -22,25 +20,22 @@
 	/**
 	 *	public
 	 */
-
 	SortableList.prototype = {
 
-		// After document is ready, init() has to be invoked
-		// to create jQuery objects and add event listeners
+		// After document is ready, init() has to be called to invoke event listeners
 		init: function(options) {
-
 			// this.items stores the reference of array of objects such as favorites
 			this.items = options.items || [];
-			// this.tempItems is a temporary storage for updating this.items
-			// this.tempItems is assigned a shallow copy of this.itmes here
+			// this.tempItems is a temporary storage for updated this.items
+			// Make this.tempItems a shallow copy of this.itmes
 			this.tempItems = this.items.slice(0);
+			// this.methods stores helper functions from outside of the module
+			this.methods = options.methods || {};
+			// event listeners
+			this.events = options.events || {};
 			// this.defaultAnimationDuration determines how long the animation will take
 			// for instance, show, hide, fadeIn, fadeOut, etc
 			this.defaultAnimationDuration = options.defaultAnimationDuration;
-			// event listeners
-			this.events = options.events || {};
-			// customized methods
-			this.methods = options.methods || {};
 
 			// Create jQuery objects by using HTML ids and classes obtained from options
 			this.$listSection = $('#' + this.listSectionId);
@@ -52,116 +47,76 @@
 			this.$detailArticle = $('#' + this.detailArticleId);
 			this.$detailEditArticle = $('#' + this.detailEditArticleId);
 
-			// this.showList();
 			this.invokeEventListeners();
-
 		},
 
-		// Add eventListeners
+		// Add eventListeners to this.events
 		addEventListeners: function(newEvents) {
-
 			$.extend(this.events, newEvents);
-
 		},
 
-		// Invoke the event listeners in this.events
+		// Invoke the event listeners
 		invokeEventListeners(events) {
-
-			// If no events parameter is provided,
-			// invoke this.events which have been added previoulsy.
+			// If no events is given, this.events will be invoked
 			events = events || this.events;
 			for (var eventName in events) {
 				console.log('Event listener, "' + eventName + '" is invoked.');
 				this.events[eventName].call(this);
 			}
-
 		},
 
 		// Create HTML elements and append them to this.$listHeader and this.$listUl
 		resetListSection: function() {
-
 			var self = this;
 
 			// Remove all existing child nodes of this.$listHeader
 			this.$listHeader.empty();
 			console.log('empty this.$listHeader with id="' + this.listHeaderId + '"');
 
-			// Create a header and append it to this.$listHeader
-			this.$listHeader.append($(this.getListHeaderHtml()));
-
-			// Remove all existing child nodes of $ul
+			// Remove all existing child nodes of this.$ul
 			this.$listUl.empty();
 			console.log('empty this.$listUl with id="' + this.listUlId + '"');
+
+			// Create a header and append it to this.$listHeader
+			this.$listHeader.append($(this.getListHeaderHtml()));
 
 			// Create an array of li HTML strings iterating through this.items
 			var arrayLiHtmls = this.items.map(function(item, index, items) {
 				return self.getListLiHtml(index);
 			});
 
-			// Create HTML lis and append them into this.$listUl
+			// Combine the array of li HTML to a string and append it to this.$listUl
 			this.$listUl.append(arrayLiHtmls.join(''));
-
 		},
 
 		// Create HTML elements and append them to this.$detailArticle
 		resetDetailArticle: function() {
-
-			var self = this;
-
 			// Remove all existing child nodes of this.$detailArticle
 			this.$detailArticle.empty();
 
 			var strDetailHtml = this.getDetailArticleHtml();
 			this.$detailArticle.append($(strDetailHtml));
-
 		},
 
 		// Create HTML elements for this.$detailEditArticle
 		resetDetailEditArticle: function() {
-
-			var self = this;
-
 			// Remove all existing child nodes of this.$detailEditArticle
 			this.$detailEditArticle.empty();
 
 			var strDetailEditHtml = this.getDetailEditArticleHtml();
 			this.$detailEditArticle.append($(strDetailEditHtml));
-
 		},
 
-		// Update this.items same as this.tempItems
-		updateItems: function(callback) {
-
-			// var items = this.items;
-			// var tempItems = this.tempItems;
-			// var tempItemsLength = tempItems.length;
-			//
-			// // Copy the array elements from tempItems to items
-			// for (var i = 0; i < tempItemsLength; i++) {
-			// 	items[i] = tempItems[i];
-			// }
-			// // Removed the elements of items which have been removed from tempItems
-			// items.splice(tempItemsLength);
-			//
-			// if (callback) {
-			// 	var itemsLength = items.length;
-			// 	for (var i = 0; i < itemsLength; i++) {
-			// 		callback.call(this, items[i], i, items);
-			// 	}
-			// }
-
-			// Make this.items a shallow copy of this.tempItems
-			this.items = this.tempItems.slice(0);
-
-			if (callback) {
-				callback.call(this, this.items);
-			}
-
+		// Set this.items point at newItems and Make this.tempItems a shallow copy of this.items
+		setBothItems: function(newItems) {
+			this.items = newItems;
+			// Make this.tempItems a shallow copy of this.items
+			this.tempItems = this.items.slice(0);
 		},
 
-		// Update this.tempItems according to the changes on this.$listUl
-		updateTempItems: function(callback) {
-
+		// Update the array, this.tempItems according to the changes on $listUl
+		// such as the changes on the order and deletion of lis of $listUl
+		saveListToTempItems: function() {
 			var items = this.items;
 			var tempItems = this.tempItems;
 			var arrayLis = $.makeArray(this.$listUl.find('.' + this.listLiClass));
@@ -176,31 +131,12 @@
 			}
 			// Removed the elements of items which have been removed from this.$listUl
 			tempItems.splice(lisLength);
-
-			if (callback) {
-				callback.call(this, tempItems);
-			}
-
-		},
-
-		// Update this.items same as this.tempItems and save this.items into database
-		saveListEdit: function(callback) {
-
-			this.updateItems();
-
-			// callback function will save this.items to database
-			if (callback) {
-				callback.call(this, this.items);
-			}
-
 		},
 
 		// Update one element of this.items accroding to the changes from the form inputs
 		// and save this.items into database
-		saveDetailEdit: function(callback) {
-
-			// var item = this.items[itemIndex];
-			var item = this.items[this.selectedItemIndex];
+		saveDetailToTempItems: function() {
+			var item = this.tempItems[this.selectedItemIndex];
 
 			// Turn an array-like jQuery object into an array
 			var arrayInputs = $.makeArray(this.$detailEditArticle.find('input'));
@@ -225,39 +161,76 @@
 				var prop = $textarea.attr('name');
 				item[prop] = $textarea.val();
 			}
+		},
 
-			// callback function will save this.items to database
-			if (callback) {
-				callback.call(this, this.items);
-			}
+		hideList: function(animationDuration) {
+			// animationDuration = animationDuration || this.defaultAnimationDuration;
+			this.$listSection.hide(animationDuration);
+		},
 
+		hideDetail: function(animationDuration) {
+			// animationDuration = animationDuration || this.defaultAnimationDuration;
+			this.$detailArticle.hide(animationDuration);
+		},
+
+		hideDetailEdit: function(animationDuration) {
+			// animationDuration = animationDuration || this.defaultAnimationDuration;
+			this.$detailEditArticle.hide(animationDuration);
+		},
+
+		showList: function(animationDuration) {
+			animationDuration = animationDuration || this.defaultAnimationDuration;
+			this.hideDetail(animationDuration);
+			this.hideDetailEdit(animationDuration);
+			this.resetListSection();
+			this.$listSection.show(animationDuration);
+		},
+
+		// Reset HTML detail contents and display the detail article
+		showDetail: function(animationDuration) {
+			animationDuration = animationDuration || this.defaultAnimationDuration;
+			this.hideList(animationDuration);
+			this.hideDetailEdit(animationDuration);
+			// Show this.$detailSection and this.$detailArticle
+			this.resetDetailArticle();
+			this.$detailSection.show(animationDuration);
+			this.$detailArticle.show(animationDuration);
+		},
+
+		// Display the edit form for the previously selected favorite detail
+		showDetailEdit: function(animationDuration) {
+			animationDuration = animationDuration || this.defaultAnimationDuration;
+			this.hideList(animationDuration);
+			this.hideDetail(animationDuration);
+			this.resetDetailEditArticle();
+			// Show this.$detailSection and this.$detailEditArticle
+			this.$detailSection.show(animationDuration);
+			this.$detailEditArticle.show(animationDuration);
 		},
 
 		// Make this.$ul sortable
 		// callback is invoked whenever there is any changes on the order of this.$ul
 		_enableListSortable: function(callback) {
-
 			var self = this;
 
 			this.$listUl.sortable({
 				disabled: false,
 				update: function() {
 					// Update this.tempItems according to the changes on this.$listUl
-					self.updateTempItems();
+					self.saveListToTempItems();
 					if (callback) {
 						callback.call(self);
 					}
 				}
 			});
+
 			// Disable selection of text content in this.$ul
 			this.$listUl.disableSelection();
 			console.log('SortableList is enabled');
-
 		},
 
 		// Make this.$ul unsortable
 		_disableListSortable: function() {
-
 			this.$listUl.sortable({
 				disabled: true,
 			});
@@ -265,74 +238,7 @@
 			// Enable selection of text content in this$.ul
 			this.$listUl.enableSelection();
 			console.log('SortableList is disabled');
-
-		},
-
-		_hideList: function(animationDuration) {
-
-			// var animationDuration = animationDuration || this.defaultAnimationDuration;
-			this.$listSection.hide(animationDuration);
-			console.log('_hideList() is called');
-
-		},
-
-		_hideDetail: function(animationDuration) {
-
-			// var animationDuration = animationDuration || this.defaultAnimationDuration;
-			this.$detailArticle.hide(animationDuration);
-			console.log('_hideDetail() is called');
-
-		},
-
-		_hideDetailEdit: function(animationDuration) {
-
-			// var animationDuration = animationDuration || this.defaultAnimationDuration;
-			this.$detailEditArticle.hide(animationDuration);
-			console.log('_hideDetailEdit() is called');
-
-		},
-
-		// Reset HTML list contents and display the list section
-		showList: function(opts) {
-
-			opts = opts || {};
-			var animationDuration = opts.animationDuration || this.defaultAnimationDuration;
-			this._hideDetail(animationDuration);
-			this._hideDetailEdit(animationDuration);
-			this.resetListSection();
-			this.$listSection.show(animationDuration);
-
-		},
-
-		// Reset HTML detail contents and display the detail article
-		showDetail: function(opts) {
-
-			opts = opts || {};
-			var animationDuration = opts.animationDuration || this.defaultAnimationDuration;
-
-			this._hideList(animationDuration);
-			this._hideDetailEdit(animationDuration);
-			this.resetDetailArticle();
-
-			// Show this.$detailSection and this.$detailArticle
-			this.$detailSection.show(animationDuration);
-			this.$detailArticle.show(animationDuration);
-		},
-
-		// Display the edit form for the previously selected favorite detail
-		showDetailEdit: function(opts) {
-
-			opts = opts || {};
-			var animationDuration = opts.animationDuration || this.defaultAnimationDuration;
-
-			this._hideList(animationDuration);
-			this._hideDetail(animationDuration);
-			this.resetDetailEditArticle();
-
-			// Show this.$detailSection and this.$detailEditArticle
-			this.$detailSection.show(animationDuration);
-			this.$detailEditArticle.show(animationDuration);
-		},
+		}
 
 		// // Find arrayMatchedLis which return true from callback and then
 		// // invoke action with the passing parameter arrayMatchedLis[i]
@@ -413,7 +319,6 @@
 	};
 
 	SortableList.create = function(options) {
-
 		// list section
 		this.listSectionId = options.listSectionId;
 		this.listHeaderId = options.listHeaderId;
@@ -435,6 +340,9 @@
 		// data attribute of each li
 		this.listDataId = options.listDataId;
 
+		// upload and download functions
+		this.uploadItems = options.uploadItems;
+		this.downloadItems = options.downloadItems;
 	};
 
 	// The 'new' keyword is not needed
